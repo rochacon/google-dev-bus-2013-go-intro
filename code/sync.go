bfe0495b98d7b631d155a5d338acdd849554a0a8
Rodrigo Chacon <rochacon@gmail.com>
2013-11-18 11:20:02 -0200

    WIP

--- code/sync.go
+++ code/sync.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "sync"
+)
+
+var Counter struct {
+    sync.RWMutex
+    x int
+}
 
 func main() {
-	fmt.Println("Hello world!")
+    wg := &sync.WaitGroup{}
+    wg.Add(3)
+    go sum(wg)
+    go sum(wg)
+    go sum(wg)
+    wg.Wait()
+    fmt.Println("x =", Counter.x)
+}
+
+func sum(wg *sync.WaitGroup) {
+    for x := 0; x < 10; x++ {
+        Counter.Lock()
+        Counter.x++
+        Counter.Unlock()
+    }
+    wg.Done()
 }
